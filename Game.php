cbe0f44a17573c24552d2f7e28b673770e5bb893
Aurélien Fontaine <afontaine@efidev.com>
2014-04-09 22:55:24 +0200

    End kata

--- Game.php
+++ Game.php
@@ -5,14 +5,67 @@
  *
  * @category    PhpStorm
  * @author     aurelien
- * @copyright  2014 Efidev 
+ * @copyright  2014 Efidev
  * @version    CVS: Id:$
  */
 
 namespace Bowling;
 
 
-class Game 
+class Game
 {
 
+    private $rolls = array();
+    private $currentRoll = 0;
+
+    public function roll($pin)
+    {
+        $this->rolls[$this->currentRoll++] = $pin;
+    }
+
+    public function score()
+    {
+        $score      = 0;
+        $frameIndex = 0;
+
+        for ($frame = 0; $frame < 10; $frame++) {
+
+            if ($this->isStrike($frameIndex)) {
+                $score += 10 + $this->rolls[$frameIndex + 1] + $this->rolls[$frameIndex + 2];
+
+                $frameIndex += 1;
+
+            } else if ($this->isSpare($frameIndex)) {
+                $score += 10 + $this->rolls[$frameIndex + 2];
+
+                $frameIndex += 2;
+            } else {
+
+                $score += $this->rolls[$frameIndex];
+                $score += $this->rolls[$frameIndex + 1];
+
+                $frameIndex = $frameIndex + 2;
+            }
+        }
+
+        return $score;
+    }
+
+    /**
+     * @param $frameIndex
+     * @return bool
+     */
+    private function isSpare($frameIndex)
+    {
+        return $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1] == 10;
+    }
+
+    /**
+     * @param $frameIndex
+     * @return bool
+     */
+    private function isStrike($frameIndex)
+    {
+        return $this->rolls[$frameIndex] == 10;
+    }
 } 
--- Tests/GameTest.php
+++ Tests/GameTest.php
@@ -5,15 +5,77 @@
  *
  * @category    PhpStorm
  * @author     aurelien
- * @copyright  2014 Efidev 
+ * @copyright  2014 Efidev
  * @version    CVS: Id:$
  */
 
 namespace Bowling\Tests;
 
 
+use Bowling\Game;
+
 class GameTest extends \PHPUnit_Framework_TestCase
 {
+
+    private $game;
+
+    protected function setUp()
+    {
+        $this->game = new Game();
+    }
+
+    public function testGutterGame()
+    {
+        $this->rollMany(20, 0);
+        $this->assertEquals(0, $this->game->score());
+    }
+
+    public function testAllOnes()
+    {
+        $this->rollMany(20, 1);
+        $this->assertEquals(20, $this->game->score());
+    }
+
+    public function testOneSpare()
+    {
+        $this->rollSpare();
+        $this->game->roll(3);
+        $this->rollMany(17, 0);
+
+        $this->assertEquals(16, $this->game->score());
+    }
+
+    public function testOneStrike()
+    {
+        $this->game->roll(10);
+        $this->game->roll(3);
+        $this->game->roll(4);
+
+        $this->rollMany(16, 0);
+
+        $this->assertEquals(24, $this->game->score());
+    }
+
+    public function testPerfectGame()
+    {
+        $this->rollMany(12,10);
+
+        $this->assertEquals(300, $this->game->score());
+    }
+
+    private function rollMany($n, $points)
+    {
+        for ($i = 0; $i < $n; $i++) {
+            $this->game->roll($points);
+        }
+    }
+
+
+    private function rollSpare()
+    {
+        $this->game->roll(5);
+        $this->game->roll(5);
+    }
 
 }
  
